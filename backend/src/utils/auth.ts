b1f2980003e5cf2a2a1dd5b2d36def7b1ee6f314
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-22 20:26:06 +0800

    feat(backend/utils/auth): add generate JWT helper

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,8 +1,21 @@
+// [IMPORT] Libraries
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+// [IMPORT] Types
+import { TokenPayload } from '../types';
+
+// * CONSTANT
+const JWT_SECRET = process.env.JWT_SECRET || '123';
 
 // ? [HELPER] Hash password
 const hashPassword = async (password: string, saltRounds: number) => {
     return await bcrypt.hash(password, saltRounds);
 }
 
-export { hashPassword };
+// ? [HELPER] Generate JWT
+const generateToken = (payload: TokenPayload, expiresIn: string = '1h'): string => {
+    return jwt.sign(payload, JWT_SECRET, { expiresIn })
+}
+
+export { hashPassword, generateToken };

78c94ff0766a3efbfc6a91eec4fe4604c549a2f5
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-22 21:19:23 +0800

    fix(backend/app): include correct prisma import

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express, { Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-import { prisma } from './../lib/prisma';
+import { prisma } from './lib/prisma';
 
 const app: Express = express();
 

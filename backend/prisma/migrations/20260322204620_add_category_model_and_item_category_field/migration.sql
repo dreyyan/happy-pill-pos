71d97a184e48574f230095c9c973dea586b98b6f
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-23 04:52:24 +0800

    feat(schema): add 'Category' model and 'category' field for 'Item' model

---
+++ backend/prisma/migrations/20260322204620_add_category_model_and_item_category_field/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `category` on the `Item` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Item" DROP COLUMN "category",
+ADD COLUMN     "categoryId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;

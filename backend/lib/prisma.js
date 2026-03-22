87b2eac2ca1e4d1570267d4de078fb0565ab56b2
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-22 17:46:13 +0800

    init: basic Node + Prisma backend setup

---
+++ backend/lib/prisma.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
+
+let prisma;
+
+if (!global.prisma) {
+  global.prisma = new PrismaClient();
+}
+
+prisma = global.prisma;
+
+module.exports = { prisma };

35e096afaed40429c137876ae42ddcd3002a18c9
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-23 20:53:57 +0800

    fix(route): broken 'Layout'routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-// [IMPORT] Navigation
+// [IMPORT] React
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-// [IMPORT] Routes
+// [IMPORT] Pages
 import AdminLogin from "./pages/auth/AdminLogin";
 import CashierLogin from "./pages/auth/CashierLogin";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import CashierDashboard from "./pages/admin/CashierDashboard";
 
+// Layout
+import Layout from "./Layout";
+
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Navigate to="/login/admin" replace />} />
+      <Route path="/" element={<Navigate to="/login/admin" replace />} />
 
-        {/* Authentication */}
-        <Route path="/login/admin" element={<AdminLogin/>} />
-        <Route path="/login/cashier" element={<CashierLogin/>} />
+      {/* Authentication */}
+      <Route path="/login/admin" element={<AdminLogin />} />
+      <Route path="/login/cashier" element={<CashierLogin />} />
 
-        {/* Dashboard */}
-        <Route path="/admin/dashboard" element={<AdminDashboard/>} />
-        <Route path="/cashier/dashboard" element={<CashierDashboard/>} />
+      {/* Protected Routes */}
+      <Route element={<Layout />}>
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/cashier/dashboard" element={<CashierDashboard />} />
+      </Route>
+
+      {/* [OPTIONAL] Catch all/404 redirect */}
+      <Route path="*" element={<Navigate to="/login/admin" replace />} />
     </Routes>
   );
 }

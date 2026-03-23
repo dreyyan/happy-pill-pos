95884f43df0fd23a0ef93bc04e5732107f002a1a
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-23 19:49:45 +0800

    feat(route): add admin and cashier dashboard routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 // [IMPORT] Routes
 import AdminLogin from "./pages/auth/AdminLogin";
 import CashierLogin from "./pages/auth/CashierLogin";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import CashierDashboard from "./pages/admin/CashierDashboard";
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
         {/* Authentication */}
         <Route path="/login/admin" element={<AdminLogin/>} />
         <Route path="/login/cashier" element={<CashierLogin/>} />
+
+        {/* Dashboard */}
+        <Route path="/admin/dashboard" element={<AdminDashboard/>} />
+        <Route path="/cashier/dashboard" element={<CashierDashboard/>} />
     </Routes>
   );
 }

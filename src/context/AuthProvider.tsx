6f2bd97fd8091d18074fb7c7fdbf2a8cc3011030
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-23 17:13:40 +0800

    feat(auth): create 'useAuth' provider for user authentication

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,43 @@
+// [IMPORT] Hooks
+import React from "react";
+import { useState, type ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./AuthContextOnly";
+
+// [IMPORT] Components
+import Modal from "../components/Modal";
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  // [STATES]
+  const [showTokenExpiredModal, setShowTokenExpiredModal] = useState(false);
+  const navigate = useNavigate();
+
+  // [HANDLE] Logout
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    setShowTokenExpiredModal(false);
+    navigate("/");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ showTokenExpiredModal, setShowTokenExpiredModal, logout }}
+    >
+      {children}
+
+      {/* [UI] Session Expired Modal */}
+      <Modal
+        isOpen={showTokenExpiredModal}
+        onClose={() => {}}
+        title="Session Expired"
+        message="Your session has expired. Please log in again."
+        confirmText="Go to Login"
+        type="error"
+        isCancelable={false}
+        closeOnBackdrop={false}
+        onConfirm={logout}
+      />
+    </AuthContext.Provider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+import { AuthProvider } from './context/AuthProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App/>
+      <AuthProvider>
+        <App/>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )

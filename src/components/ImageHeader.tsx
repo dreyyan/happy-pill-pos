a6558c8f114ea7ae32734ad5e03bde4c8ca2bc0b
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-24 18:48:42 +0800

    refactor(image-header): add missing '-' to system title

--- src/components/ImageHeader.tsx
+++ src/components/ImageHeader.tsx
@@ -16,7 +16,7 @@ const ImageHeader = () => {
 
     return (
         <button onClick={() => navigate("/")} className="relative flex flex-col justify-center items-center space-y-2 bg-gradient-to-tr from-primary-500 to-primary-700 w-full px-4 py-8 cursor-pointer">
-            <h1 className="text-text-50">Happy Pill Cafe</h1>
+            <h1 className="text-text-50">Happy-Pill Cafe</h1>
             <p className="text-h5 text-text-200">"Powerful POS for seamless sales and operations"</p>
         </button>
     );

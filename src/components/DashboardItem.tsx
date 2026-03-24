989cc99100ed4f44f1bdcaca0ffb3be9d97cb197
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-03-24 23:21:25 +0800

    style(dashboard-item): add responsiveness

--- src/components/DashboardItem.tsx
+++ src/components/DashboardItem.tsx
@@ -1,29 +1,31 @@
 import React from "react";
 
+// ? [INTERFACE]
 interface DashboardProps {
-    iconSrc?: string;
-    text: string;
-    value: number;
-};
+  iconSrc?: string;
+  text: string;
+  value: number;
+}
 
-const DashboardItem: React.FC<DashboardProps> = ({iconSrc, text, value}) => {
-    return (
-        <div className="flex items-center justify-between gap-x-2 bg-bg-50 pr-3 rounded-sm shadow-sm/10">
-            <div className="flex items-center gap-x-2">
-                {/* Icon */}
-                <div className="p-2 bg-primary-700 rounded-l-sm flex items-center justify-center">
-                <img src={iconSrc} className="size-5"/>
-                </div>
-                {/* Text */}
-                <h3 className="text-h5">{text}</h3>
-            </div>
-
-            {/* Value container */}
-            <div className="flex items-center justify-center h-full px-1 rounded-r-sm">
-                <h2 className="text-text-900 text-center">{value}</h2>
-            </div>
+const DashboardItem: React.FC<DashboardProps> = ({ iconSrc, text, value }) => {
+  return (
+    <div className="flex items-center justify-between gap-x-2 bg-bg-50 pr-3 rounded-sm shadow-sm/10 w-full max-w-[400px] sm:max-w-[500px] md:max-w-[600px] lg:max-w-[700px]">
+      <div className="flex items-center gap-x-2">
+        {/* [UI] Icon */}
+        <div className="p-2 bg-primary-700 rounded-l-sm flex items-center justify-center min-w-[2.5rem] min-h-[2.5rem]">
+          <img src={iconSrc} className="w-5 h-5 object-contain" alt={text + " icon"} />
         </div>
-    );
+
+        {/* [UI] Text */}
+        <h3 className="text-h5">{text}</h3>
+      </div>
+
+      {/* [SECTION] Value */}
+      <div className="flex items-center justify-center h-full px-1 rounded-r-sm">
+        <h2 className="text-text-900 text-center">{value}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardItem;
